db3761a6ba6cc6002257d084e90c7052ec861fb4
Gustavo Rocha <gustavofrocha7@gmail.com>
2021-02-18 22:03:49 -0300

    created  var place at events

--- src/eventss/events.entity.ts
+++ src/eventss/events.entity.ts
@@ -2,17 +2,25 @@ import {
     BaseEntity,
     Entity,
     Unique,
+    ManyToOne,
+    JoinColumn,
     PrimaryGeneratedColumn,
     Column,
     CreateDateColumn,
     UpdateDateColumn,
   } from 'typeorm';
+
+  import {Place} from 'src/places/place.entity'
   
   @Entity('eventss')
   @Unique(['name'])
   export class Events extends BaseEntity {
     @PrimaryGeneratedColumn()
     id: string;
+
+    @ManyToOne(() => Place)
+    @JoinColumn({ name: "place_id" })
+    place: Place;
   
     @Column({ nullable: false, type: 'varchar', length: 200 })
     name: string;
