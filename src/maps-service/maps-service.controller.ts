7ce514fcf07c82d728349ee470301d3f9a62dce2
Gustavo Rocha <gustavofrocha7@gmail.com>
2021-06-15 23:56:14 -0300

    🧩: added description for request

--- src/maps-service/maps-service.controller.ts
+++ src/maps-service/maps-service.controller.ts
@@ -9,8 +9,16 @@ export class RouteController {
     constructor(private routeService: RouteService){}
 
     @Get()
+    @ApiOperation({
+        summary: 'Pegando rota',
+      })
+      @ApiResponse({
+        description: 'Trajeto obtido com sucesso',
+        status: HttpStatus.OK
+        // type: CreatePlaceDTO,
+      })
     async route(): Promise<Route>{
         return await this.routeService.route()
-      }
+    }
 
 }
