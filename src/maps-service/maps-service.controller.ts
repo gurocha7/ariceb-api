53207175d3cba384a956524039d676bc30da9cfa
Gustavo Rocha <gustavofrocha7@gmail.com>
2021-06-15 22:01:20 -0300

    🧩: service file created

---
+++ src/maps-service/maps-service.controller.ts
@@ -0,0 +1,9 @@
+import { Body, Controller, Delete,HttpStatus,Param ,Post, Query, Get, Put } from '@nestjs/common';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+
+@Controller('v1/route')
+export class RouteController {
+
+    
+
+}
---
+++ src/maps-service/maps-service.service.ts
@@ -0,0 +1,9 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class RouteService {
+
+    
+
+}
