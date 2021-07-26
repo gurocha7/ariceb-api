fdd241c0de1a034148e751df20e5bde3cdc10695
Axel Junio <axeljunio@gmail.com>
2021-07-26 00:15:52 -0300

    up

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
 
-  // iniciando servidor
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
